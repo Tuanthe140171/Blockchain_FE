97f3e55ef3d82f0bbe63c43ac980dcd6aa2e6a90
= <truongvuetify@gmail.com>
2022-03-22 22:49:57 +0700

    Finish dashboard API integration

--- src/containers/Dashboard/component/DashSystem/index.tsx
+++ src/containers/Dashboard/component/DashSystem/index.tsx
@@ -4,7 +4,6 @@ import {
   ZoomInOutlined,
 } from "@ant-design/icons";
 import { Avatar, Col, Input, Row, Table, Image } from "antd";
-import { exportDataToCsv } from '../../../../utils/csvGenerator';
 import {
   ArcElement,
   CategoryScale,
@@ -23,7 +22,10 @@ import { Line, Pie } from "react-chartjs-2";
 import moment from "moment";
 import BigNumber from "bignumber.js";
 import useFetch from "../../../../hooks/useFetch";
+import useDebounce from "../../../../hooks/useDebounce";
 import { shortenTx } from "../../../../utils";
+import { plugins } from "../../../../utils/chart";
+import { exportDataToCsv } from '../../../../utils/csvGenerator';
 import "./index.scss";
 
 enum CharityStatus {
@@ -46,65 +48,30 @@ ChartJS.register(
   ArcElement
 );
 
-
-const plugins = [
-  {
-    afterDraw: (chart: { tooltip?: any; scales?: any; ctx?: any }) => {
-      // eslint-disable-next-line no-underscore-dangle
-      if (chart.tooltip && chart.tooltip._active && chart.tooltip._active.length) {
-        // find coordinates of tooltip
-        const activePoint = chart.tooltip._active[0];
-        const { ctx } = chart;
-        const { x, y } = activePoint.element;
-        const topY = chart.scales.y.top;
-        const bottomY = chart.scales.y.bottom;
-        const topX = chart.scales.x.left;
-        const bottomX = chart.scales.x.right;
-
-        ctx.lineWidth = 1;
-        ctx.setLineDash([3, 3]);
-        ctx.strokeStyle = '#EEC909';
-
-        // draw vertical line
-        ctx.save();
-        ctx.beginPath();
-        ctx.moveTo(x, topY);
-        ctx.lineTo(x, bottomY);
-        ctx.stroke();
-
-
-        // Draw horizontal line
-        ctx.beginPath();
-        ctx.moveTo(topX, y);
-        ctx.lineTo(bottomX, y);
-        ctx.stroke();
-
-        ctx.restore();
-      }
-    },
-  },
-];
-
 const DashSystem: React.FC<{
   pickedDate: {
     from: number,
     to: number
   } | undefined
 }> = (props) => {
-  console.log(props.pickedDate);
+  const [keyWord, setKeyWord] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [chartRef, setChartRef] = useState<any>();
-  const { data: transactionsResp, loading } = useFetch<any>(`transactions?page=${currentPage}`, {
+  const [userChartRef, setUserChartRef] = useState<any>();
+
+  const debouncedKeyword = useDebounce<string>(keyWord, 500)
+
+  const { data: transactionsResp, loading } = useFetch<any>(`transactions?page=${currentPage}&keyword=${debouncedKeyword}`, {
     "Content-Type": "application/json",
     Accept: "application/json"
   });
 
-  const { data: userStatsResp, loading: userStatsLoading } = useFetch<any>(`users/stats/number`, {
+  const { data: userStatsResp } = useFetch<any>(`users/stats/number`, {
     "Content-Type": "application/json",
     Accept: "application/json"
   });
 
-  const { data: dailyDonationResp, loading: dailyDonationLoading } = useFetch<any>(
+  const { data: dailyDonationResp } = useFetch<any>(
     !props.pickedDate ? `transactions/daily`: `transactions/daily?fromDate=${props.pickedDate.from}&toDate=${props.pickedDate.to}`, 
     {
     "Content-Type": "application/json",
@@ -112,40 +79,83 @@ const DashSystem: React.FC<{
     }
   );
 
-  const { data: donationResp, loading: donationRespLoading } = useFetch<any>(`landing-page/stats`, {
+  const { data: donationResp } = useFetch<any>(`landing-page/stats`, {
     "Content-Type": "application/json",
     Accept: "application/json"
   });
+
+  const { data: userActiveResp } = useFetch<any>(
+    !props.pickedDate ? `active/stats`: `active/stats?fromDate=${props.pickedDate.from}&toDate=${props.pickedDate.to}`,
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    }
+  );
 
   const charityStatus = {
     status: CharityStatus.UP,
     percentage: '0'
   }
 
+  const userStatsStatus = {
+    status: CharityStatus.UP,
+    percentage: '0',
+    totalUserActions: 0
+  }
+
   if (dailyDonationResp && dailyDonationResp.donationDayDatas.length >= 1) {
     const dayDatas = dailyDonationResp.donationDayDatas;
     if (dayDatas.length >= 2 && dayDatas[dayDatas.length - 2].dailyVolume > 0) {
-      console.log(
-        dayDatas[dayDatas.length - 1].dailyVolume,
-        dayDatas[dayDatas.length - 2].dailyVolume
-      )
       charityStatus.percentage = new BigNumber((dayDatas[dayDatas.length - 1].dailyVolume / dayDatas[dayDatas.length - 2].dailyVolume)).multipliedBy(100).toFixed(4)
     }
     charityStatus.status = new BigNumber(charityStatus.percentage).lt(1) ? CharityStatus.DOWN : CharityStatus.UP;
   }
 
+  if (userActiveResp && userActiveResp.length >= 1) {
+    if (userActiveResp.length >= 2 && userActiveResp[userActiveResp.length - 2].count > 0) {
+      userStatsStatus.percentage = new BigNumber((userActiveResp[userActiveResp.length - 1].count / userActiveResp[userActiveResp.length - 2].count)).multipliedBy(100).toFixed(4)
+    }
+    userStatsStatus.status = new BigNumber(userStatsStatus.percentage).lt(1) ? CharityStatus.DOWN : CharityStatus.UP;
+    
+    for (let userStats of userActiveResp) {
+      userStatsStatus.totalUserActions += parseInt(userStats.count);
+    }
+  }
+
+
   var gradientStroke = chartRef?.ctx?.createLinearGradient(0, 500, 0, 100);
   gradientStroke?.addColorStop(1, "rgba(238, 201, 9, 0.27)");
   gradientStroke?.addColorStop(0.3, "rgba(255, 255, 255, 0)");
+
+  var userGradientStroke = userChartRef?.ctx?.createLinearGradient(0, 500, 0, 100);
+  userGradientStroke?.addColorStop(1, "rgba(82, 191, 214, 0.3)");
+  userGradientStroke?.addColorStop(0.3, "rgba(255, 255, 255, 0)");
+  
 
   const data: ChartData<"line", any, unknown> = {
     labels: dailyDonationResp ? dailyDonationResp.donationDayDatas.map((data: any) => moment(parseInt(data.date) * 1000).format("DD-MM-yy")) : [],
     datasets: [
       {
         fill: true,
-        data: dailyDonationResp ? dailyDonationResp.donationDayDatas.map((data: any) => new BigNumber(data.dailyVolume).div(new BigNumber(10).pow(18)).toFixed()) : [],
+        data: dailyDonationResp ? dailyDonationResp.donationDayDatas.map((data: any) => new BigNumber(data.dailyVolume).div(1e18).toFixed()) : [],
         backgroundColor: gradientStroke,
         borderColor: '#EEC909',
+        tension: 0.5,
+        pointBorderWidth: 0,
+        pointHoverBorderWidth: 2,
+        pointHoverBackgroundColor: "white"
+      },
+    ],
+  };
+
+  const userStatsData: ChartData<"line", any, unknown> = {
+    labels: userActiveResp ? userActiveResp.map((stats: any) => moment(new Date(stats.date)).format("DD-MM-yy")) : [],
+    datasets: [
+      {
+        fill: true,
+        data: userActiveResp ? userActiveResp.map((stats: any) => stats.count) : [],
+        backgroundColor: userGradientStroke,
+        borderColor: '#52BFD6',
         tension: 0.5,
         pointBorderWidth: 0,
         pointHoverBorderWidth: 2,
@@ -176,13 +186,6 @@ const DashSystem: React.FC<{
       intersect: false,
       mode: 'index',
     },
-    // maintainAspectRatio: false,
-    // elements: {
-    //   point: {
-    //     radius: 5,
-    //     hoverRadius: 4,
-    //   },
-    // }
   }
 
 
@@ -227,27 +230,6 @@ const DashSystem: React.FC<{
         </div>
       ),
     },
-    // {
-    //   title: "Status",
-    //   dataIndex: "status",
-    //   render: (tags: any) => (
-    //     <>
-    //       {tags.map((tag: any) => {
-    //         let color = tag.length > 5 ? "geekblue" : "green";
-    //         if (tag === "loser") {
-    //           color = "volcano";
-    //         }
-    //         return (
-    //           <Tag color={color} key={tag}>
-    //             {tag.toUpperCase()}
-    //           </Tag>
-    //         );
-    //       })}
-    //     </>
-    //   ),
-    //   key: "status",
-    //   sorter: (a: any, b: any) => a.status - b.status,
-    // },
     {
       title: "Date",
       dataIndex: "date",
@@ -262,12 +244,12 @@ const DashSystem: React.FC<{
     },
   ];
 
-  const tableData = transactionsResp ? transactionsResp.rows.map((transaction: any) => ({
+  const transactionsTableData = transactionsResp ? transactionsResp.rows.map((transaction: any) => ({
     id: shortenTx(transaction.id),
     key: shortenTx(transaction.id),
-    name: transaction.from.name,
-    donee: transaction.to.name,
-    date: moment(new Date(parseInt(transaction.donated_at_timestamp) * 1000)).format("MM/DD/YY hh:ss"),
+    name: transaction["fromUser.name"],
+    donee: transaction["toUser.name"],
+    date: moment(new Date(transaction["date"])).format("MM/DD/YY hh:ss"),
     amount: transaction.amount,
     status: ["loser"],
   })) : [];
@@ -300,13 +282,13 @@ const DashSystem: React.FC<{
                 />
               </div>
             </div>
-            <Line ref={ref => setChartRef(ref)} data={data} plugins={plugins as any} options={options} className="chart-group__chart" />
+            <Line ref={ref => setChartRef(ref)} data={data} plugins={plugins() as any} options={options} className="chart-group__chart" />
             <div className="chart-group__data-group">
               <h3 className="chart-group__data-group__title">Total charity</h3>
               <h1 className="chart-group__data-group__data">
                 {donationResp ? new BigNumber(donationResp.donations[0].totalVolume).div(1e18).toFixed(2) : 0} CRV
                 <span className={`chart-group__data-group__data__rate chart-group__data-group__data__rate--${charityStatus.status === CharityStatus.UP ? 'up' : 'down'}`}>
-                  <Image preview={false} src={charityStatus.status === CharityStatus.UP ? '/icon/growth.svg' : '/icon/down.svg'} />
+                  <Image preview={false} src="/icon/growth.svg" className={`chart-group__data-group__data__icon--${charityStatus.status === CharityStatus.UP ? 'up' : 'down'}`} />
                   <span>{charityStatus.percentage}%</span>
                 </span>
               </h1>
@@ -324,18 +306,20 @@ const DashSystem: React.FC<{
                 />
                 <DownloadOutlined
                   style={{ fontSize: "15px", color: "black" }}
+                  onClick={() => exportDataToCsv(userActiveResp ? userActiveResp : [], 'user-active')}
                 />
               </div>
             </div>
-            <Line data={data} options={options} plugins={plugins as any} className="chart-group__chart" />
+            <Line ref={ref => setUserChartRef(ref)} data={userStatsData} options={options} plugins={plugins("#52BFD6") as any} className="chart-group__chart" />
             <div className="chart-group__data-group">
               <h3 className="chart-group__data-group__title">
                 Total user action
               </h3>
               <h1 className="chart-group__data-group__data">
-                1,234,643
-                <span className="chart-group__data-group__data__rate">
-                  32,5%
+                {userStatsStatus.totalUserActions}
+                <span className={`chart-group__data-group__data__rate chart-group__data-group__data__rate--${charityStatus.status === CharityStatus.UP ? 'up' : 'down'}`}>
+                  <Image preview={false} src="/icon/growth.svg" className={`chart-group__data-group__data__icon--${charityStatus.status === CharityStatus.UP ? 'up' : 'down'}`} />
+                  <span>{userStatsStatus.percentage}%</span>
                 </span>
               </h1>
             </div>
@@ -352,6 +336,8 @@ const DashSystem: React.FC<{
                   placeholder="Search donee, history..."
                   // onSearch={onSearch}
                   style={{ width: 230 }}
+                  value={keyWord}
+                  onChange={e => setKeyWord(e.target.value)}
                 />
                 <div className="table-group__header__right-group__icons">
                   <ZoomInOutlined
@@ -367,7 +353,7 @@ const DashSystem: React.FC<{
             </div>
             <Table
               columns={tableColumns}
-              dataSource={tableData}
+              dataSource={transactionsTableData}
               pagination={{
                 defaultPageSize: transactionsResp ? transactionsResp.limit : 10,
                 pageSize: transactionsResp ? transactionsResp.limit : 10,
--- src/containers/Dashboard/component/DashUser/index.tsx
+++ src/containers/Dashboard/component/DashUser/index.tsx
@@ -1,9 +1,11 @@
+import React, { useState } from "react";
 import {
   DownloadOutlined,
   UserOutlined,
   ZoomInOutlined,
 } from "@ant-design/icons";
-import { Avatar, Col, Input, Row, Table, Tag } from "antd";
+import { Avatar, Col, Input, Row, Table, Image } from "antd";
+import { BigNumber } from 'bignumber.js';
 import {
   CategoryScale,
   Chart as ChartJS,
@@ -15,8 +17,14 @@ import {
   Title,
   Tooltip,
 } from "chart.js";
-import React from "react";
-import { Line, Pie } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+import { useWeb3React } from "web3-react-core";
+import moment from "moment";
+import useFetch from "../../../../hooks/useFetch";
+import useDebounce from "../../../../hooks/useDebounce";
+import { shortenTx } from "../../../../utils";
+import { exportDataToCsv } from '../../../../utils/csvGenerator';
+import { plugins } from "../../../../utils/chart";
 import "./index.scss";
 
 const { Search } = Input;
@@ -31,54 +39,139 @@ ChartJS.register(
   Legend
 );
 
-const DashUser = () => {
+enum CharityStatus {
+  UP,
+  DOWN,
+  UNDECIDED
+}
+
+const DashUser: React.FC<{
+  pickedDate: {
+    from: number,
+    to: number
+  } | undefined
+}> = (props) => {
+  const { account } = useWeb3React();
+  const [keyWord, setKeyWord] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [chartRef, setChartRef] = useState<any>();
+  const [secChartRef, setSecChartRef] = useState<any>();
+
+  const debouncedKeyword = useDebounce<string>(keyWord, 500)
+
+  const { data: transactionsResp, loading } = useFetch<any>(`transactions?page=${currentPage}&userAddress=${account}&keyword=${debouncedKeyword}`, {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  });
+
+  const { data: dailyDonationResp } = useFetch<any>(
+    !props.pickedDate ? `transactions/daily/user?userAddress=${account}`: `transactions/daily/user?userAddress=${account}&fromDate=${props.pickedDate.from}&toDate=${props.pickedDate.to}`, 
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+  });
+
+  const givingStatus = {
+    status: CharityStatus.UP,
+    percentage: '0',
+    totalGiving: 0
+  }
+
+  const receivingStatus = {
+    status: CharityStatus.UP,
+    percentage: '0',
+    totalReceiving: 0
+  }
+
+  if (dailyDonationResp && dailyDonationResp.userDonationDayDatas.length >= 1) {
+    const dayDatas = dailyDonationResp.userDonationDayDatas;
+    const totalReceives = dayDatas.map((data: any) => parseInt(data.totalReceive));
+    const totalGivings = dayDatas.map((data: any) =>  parseInt(data.totalDonation));
+
+    if (totalReceives.length >= 2 && totalReceives[totalReceives.length - 2] > 0) {
+      receivingStatus.percentage = new BigNumber((totalReceives[totalReceives.length - 1] / totalReceives[totalReceives.length - 2])).multipliedBy(100).toFixed(4)
+    }
+    receivingStatus.status = new BigNumber(receivingStatus.percentage).lt(1) ? CharityStatus.DOWN : CharityStatus.UP;
+
+    if (totalGivings.length >= 2 && totalGivings[totalGivings.length - 2] > 0) {
+      givingStatus.percentage = new BigNumber((totalGivings[totalGivings.length - 1] / totalGivings[totalGivings.length - 2])).multipliedBy(100).toFixed(4)
+    }
+    givingStatus.status = new BigNumber(receivingStatus.percentage).lt(1) ? CharityStatus.DOWN : CharityStatus.UP;
+  }
+
+  const transactionsTableData = transactionsResp ? transactionsResp.rows.map((transaction: any) => ({
+    id: shortenTx(transaction.id),
+    key: shortenTx(transaction.id),
+    name: transaction["fromUser.name"],
+    donee: transaction["toUser.name"],
+    date: moment(new Date(transaction["date"])).format("MM/DD/YY hh:ss"),
+    amount: transaction.amount,
+    status: ["loser"],
+  })) : [];
+
+  var gradientStroke = chartRef?.ctx?.createLinearGradient(0, 500, 0, 100);
+  gradientStroke?.addColorStop(1, "rgba(238, 201, 9, 0.27)");
+  gradientStroke?.addColorStop(0.3, "rgba(255, 255, 255, 0)");
+
+  var userGradientStroke = secChartRef?.ctx?.createLinearGradient(0, 500, 0, 100);
+  userGradientStroke?.addColorStop(1, "rgba(82, 191, 214, 0.3)");
+  userGradientStroke?.addColorStop(0.3, "rgba(255, 255, 255, 0)");
+
   const data: ChartData<"line", any, unknown> = {
-    labels: [1, 5, 10, 15, 20, 25],
-    // backgroundColor: ["rgba(255,0,0,1)"],
-    // lineTension: 1,
+    labels: dailyDonationResp ? dailyDonationResp.userDonationDayDatas.map((data: any) => moment(parseInt(data.date) * 1000).format("DD-MM-yy")) : [],
     datasets: [
       {
-        label: "HSN",
-        fill: false,
-        borderColor: "rgba(255, 0, 0, 0.3)",
-        pointRadius: 2,
-        data: [65, 59, 80, 81, 56, 55, 40],
-        cubicInterpolationMode: "monotone",
-        tension: 0.4,
+        fill: true,
+        data: dailyDonationResp ? dailyDonationResp.userDonationDayDatas.map((data: any) => data.totalDonation) : [],
+        backgroundColor: gradientStroke,
+        borderColor: '#EEC909',
+        tension: 0.5,
+        pointBorderWidth: 0,
+        pointHoverBorderWidth: 2,
+        pointHoverBackgroundColor: "white"
       },
     ],
   };
 
-  var options = {
-    legend: {
-      position: "right",
-      labels: {
-        boxWidth: 10,
+  const receivingData: ChartData<"line", any, unknown> = {
+    labels: dailyDonationResp ? dailyDonationResp.userDonationDayDatas.map((data: any) => moment(parseInt(data.date) * 1000).format("DD-MM-yy")) : [],
+    datasets: [
+      {
+        fill: true,
+        data: dailyDonationResp ? dailyDonationResp.userDonationDayDatas.map((data: any) => new BigNumber(data.totalReceive).div(1e18).toFixed()) : [],
+        backgroundColor: userGradientStroke,
+        borderColor: '#52BFD6',
+        tension: 0.5,
+        pointBorderWidth: 0,
+        pointHoverBorderWidth: 2,
+        pointHoverBackgroundColor: "white"
       },
+    ],
+  };
+  
+  var options: any = {
+    plugins: {
+      legend: {
+        display: false
+      }
     },
     scales: {
-      xAxes: [
-        {
-          ticks: { display: false },
-        },
-        {
-          gridLines: {
-              display:false
-          }
+      x: {
+        grid: {
+          display: false
         }
-      ],
-      yAxes: [
-        {
-          ticks: { display: false },
-        },
-        {
-          gridLines: {
-              display:false
-          }
+      },
+      y: {
+        grid: {
+          display: false
         }
-      ],
+      },
     },
-  };
+    interaction: {
+      intersect: false,
+      mode: 'index',
+    },
+  }
 
   const tableColumns: any = [
     {
@@ -121,27 +214,27 @@ const DashUser = () => {
         </div>
       ),
     },
-    {
-      title: "Status",
-      dataIndex: "status",
-      render: (tags: any) => (
-        <>
-          {tags.map((tag: any) => {
-            let color = tag.length > 5 ? "geekblue" : "green";
-            if (tag === "loser") {
-              color = "volcano";
-            }
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })}
-        </>
-      ),
-      key: "status",
-      sorter: (a: any, b: any) => a.status - b.status,
-    },
+    // {
+    //   title: "Status",
+    //   dataIndex: "status",
+    //   render: (tags: any) => (
+    //     <>
+    //       {tags.map((tag: any) => {
+    //         let color = tag.length > 5 ? "geekblue" : "green";
+    //         if (tag === "loser") {
+    //           color = "volcano";
+    //         }
+    //         return (
+    //           <Tag color={color} key={tag}>
+    //             {tag.toUpperCase()}
+    //           </Tag>
+    //         );
+    //       })}
+    //     </>
+    //   ),
+    //   key: "status",
+    //   sorter: (a: any, b: any) => a.status - b.status,
+    // },
     {
       title: "Date",
       dataIndex: "date",
@@ -155,68 +248,6 @@ const DashUser = () => {
       sorter: (a: any, b: any) => a.amount - b.amount,
     },
   ];
-
-  const tableData = [
-    {
-      id: "1",
-      key: "1",
-      name: "John Brown",
-      donee: "A",
-      status: ["loser"],
-      date: "20/12/2022",
-      amount: "11,234,678 ETH",
-    },
-    {
-      id: "2",
-
-      key: "2",
-      name: "John Brown",
-      donee: "A",
-      status: ["loser"],
-      date: "20/12/2022",
-      amount: "11,234,678 ETH",
-    },
-    {
-      id: "3",
-      key: "3",
-      name: "John Brown",
-      donee: "A",
-      status: ["loser"],
-      date: "20/12/2022",
-      amount: "11,234,678 ETH",
-    },
-    {
-      id: "4",
-      key: "4",
-      name: "John Brown",
-      donee: "A",
-      status: ["loser"],
-      date: "20/12/2022",
-      amount: "11,234,678 ETH",
-    },
-    {
-      id: "5",
-      key: "5",
-      name: "John Brown",
-      donee: "A",
-      status: ["loser"],
-      date: "20/12/2022",
-      amount: "11,234,678 ETH",
-    },
-    {
-      id: "6",
-      key: "6",
-      name: "John Brown",
-      donee: "A",
-      status: ["loser"],
-      date: "20/12/2022",
-      amount: "11,234,678 ETH",
-    },
-  ];
-
-  // function onChange(pagination, filters, sorter, extra) {
-  //   console.log("params", pagination, filters, sorter, extra);
-  // }
 
   return (
     <div className="dash-system">
@@ -232,16 +263,18 @@ const DashUser = () => {
                 />
                 <DownloadOutlined
                   style={{ fontSize: "15px", color: "black" }}
+                  onClick={() => exportDataToCsv(dailyDonationResp ? dailyDonationResp.userDonationDayDatas : [], 'daily-donation')}
                 />
               </div>
             </div>
-            <Line data={data} className="chart-group__chart" />
+            <Line data={data} ref={ref => setChartRef(ref)} options={options} plugins={plugins() as any} className="chart-group__chart" />
             <div className="chart-group__data-group">
-              <h3 className="chart-group__data-group__title">Total charity</h3>
+              <h3 className="chart-group__data-group__title">Total Giving</h3>
               <h1 className="chart-group__data-group__data">
                 1,307,000 ETH
-                <span className="chart-group__data-group__data__rate">
-                  32,5%
+                <span className={`chart-group__data-group__data__rate chart-group__data-group__data__rate--${givingStatus.status === CharityStatus.UP ? 'up' : 'down'}`}>
+                  <Image preview={false} src="/icon/growth.svg" className={`chart-group__data-group__data__icon--${givingStatus.status === CharityStatus.UP ? 'up' : 'down'}`} />
+                  <span>{givingStatus.percentage}%</span>
                 </span>
               </h1>
             </div>
@@ -258,18 +291,20 @@ const DashUser = () => {
                 />
                 <DownloadOutlined
                   style={{ fontSize: "15px", color: "black" }}
+                  onClick={() => exportDataToCsv(dailyDonationResp ? dailyDonationResp.userDonationDayDatas : [], 'daily-donation')}
                 />
               </div>
             </div>
-            <Line data={data} className="chart-group__chart" />
+            <Line data={receivingData} options={options} ref={ref => setSecChartRef(ref)} plugins={plugins("#52BFD6") as any} className="chart-group__chart" />
             <div className="chart-group__data-group">
               <h3 className="chart-group__data-group__title">
-                Total user action
+                Total receiving
               </h3>
               <h1 className="chart-group__data-group__data">
                 1,234,643
-                <span className="chart-group__data-group__data__rate">
-                  32,5%
+                <span className={`chart-group__data-group__data__rate chart-group__data-group__data__rate--${receivingStatus.status === CharityStatus.UP ? 'up' : 'down'}`}>
+                  <Image preview={false} src="/icon/growth.svg" className={`chart-group__data-group__data__icon--${receivingStatus.status === CharityStatus.UP ? 'up' : 'down'}`} />
+                  <span>{receivingStatus.percentage}%</span>
                 </span>
               </h1>
             </div>
@@ -286,6 +321,8 @@ const DashUser = () => {
                   placeholder="Search donee, history..."
                   // onSearch={onSearch}
                   style={{ width: 230 }}
+                  value={keyWord}
+                  onChange={e => setKeyWord(e.target.value)}
                 />
                 <div className="table-group__header__right-group__icons">
                   <ZoomInOutlined
@@ -300,8 +337,15 @@ const DashUser = () => {
             </div>
             <Table
               columns={tableColumns}
-              dataSource={tableData}
-              // onChange={onCha}
+              dataSource={transactionsTableData}
+              pagination={{
+                defaultPageSize: transactionsResp ? transactionsResp.limit : 10,
+                pageSize: transactionsResp ? transactionsResp.limit : 10,
+                total: transactionsResp ? transactionsResp.count : 0,
+                current: currentPage
+              }}
+              onChange={((props: any) => setCurrentPage(props.current))}
+              loading={loading}
             />
           </div>
         </Col>
