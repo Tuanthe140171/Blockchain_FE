4aa82901568c56f867c05e7d593fbb24caf12779
FGO-IRED\Admin <lightsaber2710@gmail.com>
2022-05-04 09:52:25 +0700

    update contact

--- src/containers/ContactUs/index.tsx
+++ src/containers/ContactUs/index.tsx
@@ -1,40 +1,46 @@
-import { Typography, Image } from 'antd';
-import React from 'react';
+import { Typography, Image } from "antd";
+import React from "react";
 import "./index.scss";
 
 const ContactUs: React.FC = () => {
-    return (
-        <div className="contact-us">
-            <Typography.Title level={3} className="contact-us__title">
-                Thông tin liên hệ
-            </Typography.Title>
-            <div className="contact-us__content">
-                <div className="contact-us__bg">
-                    <Image src="/icon/top-charity.svg" preview={false} />
-                </div>
-                <div className="contact-us__block">
-                    <span>Tên công ty: </span>
-                    <div>Công ty 3 chú ngỗng</div>
-                </div>
-                <div className="contact-us__block">
-                    <span>CEO: </span>
-                    <div>Nguyễn Quang Trường</div>
-                </div>
-                <div className="contact-us__block">
-                    <span>Số điện thoại: </span>
-                    <div>0332089984</div>
-                </div>
-                <div className="contact-us__block">
-                    <span>Địa chỉ: </span>
-                    <div>Số 5 ngõ 80 Trung Kính Trung Hòa Hà Nội</div>
-                </div>
-                <div className="contact-us__block">
-                    <span>Gmail: </span>
-                    <div><a href="#">truong020900@gmail.com</a></div>
-                </div>
-            </div>
+  return (
+    <div className="contact-us">
+      <Typography.Title level={3} className="contact-us__title">
+        Thông tin liên hệ
+      </Typography.Title>
+      <div className="contact-us__content">
+        <div className="contact-us__bg">
+          <Image src="/icon/top-charity.svg" preview={false} />
         </div>
-    )
-}
+        <div className="contact-us__block">
+          <span>Người liên hệ: </span>
+          <div>Trần Quý Ban</div>
+        </div>
+        <div className="contact-us__block">
+          <span>Số điện thoại: </span>
+          <div>0988091804</div>
+        </div>
+        <div className="contact-us__block">
+          <span>Gmail: </span>
+          <div>
+            <a href="#">quybanbk@gmail.com</a>
+          </div>
+        </div>
+        <div className="contact-us__block">
+          <span>Tên công ty: </span>
+          <div>Công ty cổ phần tư vấn và đào tạo Mặt Trời Xanh</div>
+        </div>  
+        <div className="contact-us__block">
+          <span>Mã số thuế: </span>
+          <div>0107160133</div>
+        </div>
+        <div className="contact-us__block">
+          <span>Địa chỉ: </span>
+          <div>Tầng 4 Tòa nhà D2 Giảng Võ, Ba Đình, Hà Nội</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ContactUs;
